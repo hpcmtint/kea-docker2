7b0dc0ff22c1cf603cc789b0d1d292c419a55d19
mgodzina <mgodzina@localhost.localdomain>
2023-08-24 08:36:40 +0200

    [#14] First oneclick implementation

---
+++ oneclickkea/build_images.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker build --tag kea-dhcp4 --build-arg VERSION=2.3.8-r20230530063557 - < ../docker/kea-dhcp4.Dockerfile
+wget -q https://gitlab.isc.org/isc-projects/kea/raw/Kea-2.3.8/src/share/database/scripts/pgsql/dhcpdb_create.pgsql -O ./initdb/dhcpdb_create.pgsql
